30ec5c013a18b107edb22a3a5d127bd6e04ee9e3
Nelson Cadenas <nca1478@yahoo.com>
2021-08-14 17:03:29 -0400

    02-003 Next Link & Router

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,13 @@
+import { useRouter } from 'next/dist/client/router'
+
 export default function EventPage() {
+  const router = useRouter()
+
   return (
     <div>
       <h1>My Event</h1>
+      <h3>{router.query.slug}</h3>
+      <button onClick={() => router.push('/')}>Click</button>
     </div>
   )
 }
