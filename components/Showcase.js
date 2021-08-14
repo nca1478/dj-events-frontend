c0638f5c3fe858731a76aad2ff8a73c6d61416a5
Nelson Cadenas <nca1478@yahoo.com>
2021-08-14 18:39:59 -0400

    02-008 Showcase Component

---
+++ components/Showcase.js
@@ -0,0 +1,12 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div>
+      <div className={styles.showcase}>
+        <h1>Welcome to the Party</h1>
+        <h2>Find the Hottest DJ Events</h2>
+      </div>
+    </div>
+  )
+}
